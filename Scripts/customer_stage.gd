819a33a5248d2234d4c42a957aa873ee5ea46890
Joel Clapp <psychic.apparatus@gmail.com>
2024-06-18 21:01:15 -0400

    Completed Single Day
    Game plays through the first day, but doesn't loop. Customer evaluation now prints to the text box and customers leave the store after making their purchase. Refactored so customer evaluation is owned by the customer stager not the main game script.

--- Scripts/customer_stage.gd
+++ Scripts/customer_stage.gd
@@ -1,5 +1,7 @@
 extends Control
 
+# TO-DO Script is getting large, consider refactoring and breaking debugger into it's own scene
+
 @export var queue_resource : CustomerQueue
 
 @onready var spawn_point = $SpawnPoint
@@ -7,7 +9,7 @@ extends Control
 @onready var buttons : VBoxContainer = $VBoxContainer
 @onready var customer_queue : Array[CustomerResource] = queue_resource.customer_queue
 
-
+var item_sold : Item
 var customer = load("res://Scenes/customer.tscn")
 var cool_attribute = load("res://ItemAttributes/ia_cool.tres")
 var current_customer : Customer
@@ -18,6 +20,7 @@ func _ready() -> void:
 	# TO-DO refactor so debugger mode off is default state, not set on ready
 	debugger_deactivate()
 	SignalManager.start_day.connect(_on_start_day)
+	SignalManager.item_sold.connect(_on_item_sold)
 
 
 func prestage_customer(customer_data : CustomerResource) -> void:
@@ -28,9 +31,11 @@ func prestage_customer(customer_data : CustomerResource) -> void:
 	next_customer.item_preference = customer_data.item_preference
 
 
-func new_customer(customer_data : CustomerResource) -> void:
+func new_customer() -> void:
 	free_customer()
+	
 	if customer_queue.is_empty() == false:
+		var customer_data = customer_queue.pop_front()
 		prestage_customer(customer_data)
 		current_customer = next_customer
 		spawn_point.add_child(current_customer)
@@ -62,34 +67,72 @@ func free_customer():
 	
 
 func _on_start_day() -> void:
-	new_customer(customer_queue.pop_front())
+	new_customer()
+
+
+func evaluate_item():
+	# Flag for item preference match
+	var approved : bool = false
+	
+	# Check item attributes and customer preferences for any matches
+	for ia in item_sold.item_attributes:
+		for ip in current_customer.item_preference:
+			if ia == ip:
+				approved = true 
+	
+	# If matches, customer is happy
+	if approved:
+		text_box.text = "They liked it!"
+		await get_tree().create_timer(3.0).timeout
+		free_customer()
+		await get_tree().create_timer(3.0).timeout
+		new_customer()
+		
+	else:
+		text_box.text ="I'm not sure they liked that..."
+		await get_tree().create_timer(3.0).timeout
+		free_customer()
+		await get_tree().create_timer(3.0).timeout
+		new_customer()
+
+
+func _on_item_sold(price : int, item : Item):
+	print(item.name)
+	item_sold = item
+	evaluate_item()
 
 
 # ----------- DEBUGGER UI ----------- 
 
 
 func _on_cool_customer_pressed():
-	new_customer(load("res://Data/Customers/CustomerCool.tres"))
+	customer_queue.push_front(load("res://Data/Customers/CustomerCool.tres"))
+	new_customer()
 
 
 func _on_cute_customer_pressed():
-	new_customer(load("res://Data/Customers/CustomerCute.tres"))
+	customer_queue.push_front(load("res://Data/Customers/CustomerCute.tres"))
+	new_customer()
 	
 
 func _on_flashy_customer_pressed():
-	new_customer(load("res://Data/Customers/CustomerFlashy.tres"))
+	customer_queue.push_front(load("res://Data/Customers/CustomerFlashy.tres"))
+	new_customer()
 
 
 func _on_plain_customer_pressed():
-	new_customer(load("res://Data/Customers/CustomerPlain.tres"))
+	customer_queue.push_front(load("res://Data/Customers/CustomerPlain.tres"))
+	new_customer()
 
 
 func _on_solid_customer_pressed():
-	new_customer(load("res://Data/Customers/CustomerSolid.tres"))
+	customer_queue.push_front(load("res://Data/Customers/CustomerSolid.tres"))
+	new_customer()
 
 
 func _on_indifferent_customer_pressed():
-	new_customer(load("res://Data/Customers/CustomerIndifferent.tres"))
+	customer_queue.push_front(load("res://Data/Customers/CustomerIndifferent.tres"))
+	new_customer()
 
 
 func _on_free_customer_pressed():
@@ -97,4 +140,4 @@ func _on_free_customer_pressed():
 
 
 func _on_next_customer_pressed():
-	new_customer(customer_queue.pop_front())
+	new_customer()
