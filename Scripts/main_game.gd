4325907e9337efe397f326aad77bf60f211463df
thedreadbaron <78012008+thedreadbaron@users.noreply.github.com>
2024-06-22 10:14:18 -0700

    BGM buttons
    Added UI buttons that cycle through the BGM tracks. Music icon will stop and play tracks. Tracks will automatically cycle through track array when tracks finish.

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -4,6 +4,13 @@ extends Node3D
 @onready var customer_stager = $UIRoot/CustomerStager
 @onready var inventory_dialogue : InventoryDialog = %InventoryDialog
 
+@onready var bgm = $BGM
+const TRACK_1 = preload("res://Assets/Audio/SSJ2024-Music-SFX/Music/track1.ogg")
+const TRACK_2 = preload("res://Assets/Audio/SSJ2024-Music-SFX/Music/track2.ogg")
+const TRACK_3 = preload("res://Assets/Audio/SSJ2024-Music-SFX/Music/track3.ogg")
+var bgmTracks : Array = [TRACK_1, TRACK_2, TRACK_3]
+var bgm_index : int = 1
+
 var day_index : int = -1
 var current_day : DayResource
 
@@ -18,7 +25,40 @@ func _on_start_day():
 		inventory_dialogue.open_inv_dialog(current_day.inventory)
 		customer_stager.begin_new_queue(current_day.customer_queue)
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalManager.start_day.connect(_on_start_day)
+
+
+func _on_bgm_next_pressed():
+	if bgm_index < 2:
+		bgm_index += 1
+	elif bgm_index >= 2:
+		bgm_index = 0
+	bgm.stream = bgmTracks[bgm_index]
+	bgm.play()
+
+
+func _on_bgm_prev_pressed():
+	if bgm_index > 0:
+		bgm_index -= 1
+	elif bgm_index <= 0:
+		bgm_index = bgmTracks.size() - 1
+	bgm.stream = bgmTracks[bgm_index]
+	bgm.play()
+
+
+func _on_music_toggle_pressed():
+	if bgm.playing == true:
+		bgm.stop()
+	elif bgm.playing == false:
+		bgm.play()
+
+
+func _on_bgm_finished():
+	if bgm_index < 2:
+		bgm_index += 1
+	elif bgm_index >= 2:
+		bgm_index = 0
+	bgm.stream = bgmTracks[bgm_index]
+	bgm.play()
